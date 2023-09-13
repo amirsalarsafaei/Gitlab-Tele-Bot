69eafd8950dff0531d05df7a1cf9177cd52bf1a0
Amirsalarsafaei <amirsalar.safaei@divar.ir>
2023-09-14 01:26:44 +0330

    testing gitlab body structure

--- internal/merge/notifier.go
+++ internal/merge/notifier.go
@@ -1,7 +1,19 @@
 package merge
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
 
 func (h Handler) Notifier(resp http.ResponseWriter, req *http.Request) {
+	var reqJson map[string]interface{}
+	err := json.NewDecoder(req.Body).Decode(&reqJson)
+	if err != nil {
+		log.WithError(err).Error("merge notifier failed")
+	}
 
+	fmt.Println(reqJson)
 }
