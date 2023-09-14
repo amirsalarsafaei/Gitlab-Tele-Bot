a2524188bc2cf052f115b2f44f216401e93466be
Amirsalarsafaei <amirsalar.safaei@divar.ir>
2023-09-14 18:26:08 +0330

    fixed event struct

--- internal/merge/event.go
+++ internal/merge/event.go
@@ -7,110 +7,117 @@ import (
 )
 
 type Event struct {
-	EventType  string `json:"event_type"`
 	ObjectKind string `json:"object_kind"`
+	EventType  string `json:"event_type"`
 
 	Attributes struct {
-		AssigneeId interface {
-		} `json:"assignee_id"`
-		AssigneeIds []interface {
-		} `json:"assignee_ids"`
-		AuthorId                    int       `json:"author_id"`
-		BlockingDiscussionsResolved bool      `json:"blocking_discussions_resolved"`
-		CreatedAt                   time.Time `json:"created_at"`
-		Description                 string    `json:"description"`
-		HeadPipelineId              int       `json:"head_pipeline_id"`
-		HumanTimeChange             interface {
-		} `json:"human_time_change"`
-		HumanTimeEstimate interface {
-		} `json:"human_time_estimate"`
-		HumanTotalTimeSpent interface {
-		} `json:"human_total_time_spent"`
-		Id     int `json:"id"`
-		Iid    int `json:"iid"`
-		Labels []interface {
-		} `json:"labels"`
-		LastCommit struct {
-			Author struct {
-				Email string `json:"email"`
-				Name  string `json:"name"`
-			} `json:"author"`
-			Id        string    `json:"id"`
-			Message   string    `json:"message"`
-			Timestamp time.Time `json:"timestamp"`
-			Title     string    `json:"title"`
-			Url       string    `json:"url"`
-		} `json:"last_commit"`
-		LastEditedAt interface {
-		} `json:"last_edited_at"`
-		LastEditedById interface {
-		} `json:"last_edited_by_id"`
-		MergeCommitSha string `json:"merge_commit_sha"`
-		MergeError     interface {
-		} `json:"merge_error"`
-		MergeParams struct {
+		AssigneeId     int         `json:"assignee_id"`
+		AuthorId       int         `json:"author_id"`
+		CreatedAt      string      `json:"created_at"`
+		Description    string      `json:"description"`
+		HeadPipelineId int         `json:"head_pipeline_id"`
+		Id             int         `json:"id"`
+		Iid            int         `json:"iid"`
+		LastEditedAt   string      `json:"last_edited_at"`
+		LastEditedById int         `json:"last_edited_by_id"`
+		MergeCommitSha string      `json:"merge_commit_sha"`
+		MergeError     interface{} `json:"merge_error"`
+		MergeParams    struct {
 			ForceRemoveSourceBranch string `json:"force_remove_source_branch"`
 		} `json:"merge_params"`
-		MergeStatus string `json:"merge_status"`
-		MergeUserId interface {
-		} `json:"merge_user_id"`
-		MergeWhenPipelineSucceeds bool `json:"merge_when_pipeline_succeeds"`
-		MilestoneId               interface {
-		} `json:"milestone_id"`
-		Source struct {
-			AvatarUrl interface {
-			} `json:"avatar_url"`
-			CiConfigPath      string `json:"ci_config_path"`
-			DefaultBranch     string `json:"default_branch"`
-			Description       string `json:"description"`
-			GitHttpUrl        string `json:"git_http_url"`
-			GitSshUrl         string `json:"git_ssh_url"`
-			Homepage          string `json:"homepage"`
-			HttpUrl           string `json:"http_url"`
-			Id                int    `json:"id"`
-			Name              string `json:"name"`
-			Namespace         string `json:"namespace"`
-			PathWithNamespace string `json:"path_with_namespace"`
-			SshUrl            string `json:"ssh_url"`
-			Url               string `json:"url"`
-			VisibilityLevel   int    `json:"visibility_level"`
-			WebUrl            string `json:"web_url"`
+		MergeStatus               string      `json:"merge_status"`
+		MergeUserId               interface{} `json:"merge_user_id"`
+		MergeWhenPipelineSucceeds bool        `json:"merge_when_pipeline_succeeds"`
+		MilestoneId               interface{} `json:"milestone_id"`
+		SourceBranch              string      `json:"source_branch"`
+		SourceProjectId           int         `json:"source_project_id"`
+		StateId                   int         `json:"state_id"`
+		TargetBranch              string      `json:"target_branch"`
+		TargetProjectId           int         `json:"target_project_id"`
+		TimeEstimate              int         `json:"time_estimate"`
+		Title                     string      `json:"title"`
+		UpdatedAt                 string      `json:"updated_at"`
+		UpdatedById               int         `json:"updated_by_id"`
+		Url                       string      `json:"url"`
+		Source                    struct {
+			Id                int         `json:"id"`
+			Name              string      `json:"name"`
+			Description       string      `json:"description"`
+			WebUrl            string      `json:"web_url"`
+			AvatarUrl         interface{} `json:"avatar_url"`
+			GitSshUrl         string      `json:"git_ssh_url"`
+			GitHttpUrl        string      `json:"git_http_url"`
+			Namespace         string      `json:"namespace"`
+			VisibilityLevel   int         `json:"visibility_level"`
+			PathWithNamespace string      `json:"path_with_namespace"`
+			DefaultBranch     string      `json:"default_branch"`
+			CiConfigPath      string      `json:"ci_config_path"`
+			Homepage          string      `json:"homepage"`
+			Url               string      `json:"url"`
+			SshUrl            string      `json:"ssh_url"`
+			HttpUrl           string      `json:"http_url"`
 		} `json:"source"`
-		SourceBranch    string `json:"source_branch"`
-		SourceProjectId int    `json:"source_project_id"`
-		State           string `json:"state"`
-		StateId         int    `json:"state_id"`
-		Target          struct {
-			AvatarUrl interface {
-			} `json:"avatar_url"`
-			CiConfigPath      string `json:"ci_config_path"`
-			DefaultBranch     string `json:"default_branch"`
-			Description       string `json:"description"`
-			GitHttpUrl        string `json:"git_http_url"`
-			GitSshUrl         string `json:"git_ssh_url"`
-			Homepage          string `json:"homepage"`
-			HttpUrl           string `json:"http_url"`
-			Id                int    `json:"id"`
-			Name              string `json:"name"`
-			Namespace         string `json:"namespace"`
-			PathWithNamespace string `json:"path_with_namespace"`
-			SshUrl            string `json:"ssh_url"`
-			Url               string `json:"url"`
-			VisibilityLevel   int    `json:"visibility_level"`
-			WebUrl            string `json:"web_url"`
+		Target struct {
+			Id                int         `json:"id"`
+			Name              string      `json:"name"`
+			Description       string      `json:"description"`
+			WebUrl            string      `json:"web_url"`
+			AvatarUrl         interface{} `json:"avatar_url"`
+			GitSshUrl         string      `json:"git_ssh_url"`
+			GitHttpUrl        string      `json:"git_http_url"`
+			Namespace         string      `json:"namespace"`
+			VisibilityLevel   int         `json:"visibility_level"`
+			PathWithNamespace string      `json:"path_with_namespace"`
+			DefaultBranch     string      `json:"default_branch"`
+			CiConfigPath      string      `json:"ci_config_path"`
+			Homepage          string      `json:"homepage"`
+			Url               string      `json:"url"`
+			SshUrl            string      `json:"ssh_url"`
+			HttpUrl           string      `json:"http_url"`
 		} `json:"target"`
-		TargetBranch    string    `json:"target_branch"`
-		TargetProjectId int       `json:"target_project_id"`
-		TimeChange      int       `json:"time_change"`
-		TimeEstimate    int       `json:"time_estimate"`
-		Title           string    `json:"title"`
-		TotalTimeSpent  int       `json:"total_time_spent"`
-		UpdatedAt       time.Time `json:"updated_at"`
-		UpdatedById     interface {
-		} `json:"updated_by_id"`
-		Url            string `json:"url"`
-		WorkInProgress bool   `json:"work_in_progress"`
+		LastCommit struct {
+			Id        string    `json:"id"`
+			Message   string    `json:"message"`
+			Title     string    `json:"title"`
+			Timestamp time.Time `json:"timestamp"`
+			Url       string    `json:"url"`
+			Author    struct {
+				Name  string `json:"name"`
+				Email string `json:"email"`
+			} `json:"author"`
+		} `json:"last_commit"`
+		WorkInProgress              bool          `json:"work_in_progress"`
+		TotalTimeSpent              int           `json:"total_time_spent"`
+		TimeChange                  int           `json:"time_change"`
+		HumanTotalTimeSpent         interface{}   `json:"human_total_time_spent"`
+		HumanTimeChange             interface{}   `json:"human_time_change"`
+		HumanTimeEstimate           interface{}   `json:"human_time_estimate"`
+		AssigneeIds                 []int         `json:"assignee_ids"`
+		Labels                      []interface{} `json:"labels"`
+		State                       string        `json:"state"`
+		BlockingDiscussionsResolved bool          `json:"blocking_discussions_resolved"`
+		Action                      string        `json:"action"`
 	} `json:"object_attributes"`
+
+	Labels  []interface{} `json:"labels"`
+	Changes struct {
+		StateId struct {
+			Previous int `json:"previous"`
+			Current  int `json:"current"`
+		} `json:"state_id"`
+		UpdatedAt struct {
+			Previous string `json:"previous"`
+			Current  string `json:"current"`
+		} `json:"updated_at"`
+	} `json:"changes"`
+
+	Assignees []struct {
+		Id        int    `json:"id"`
+		Name      string `json:"name"`
+		Username  string `json:"username"`
+		AvatarUrl string `json:"avatar_url"`
+		Email     string `json:"email"`
+	} `json:"assignees"`
 
 	gitlab.User
 
